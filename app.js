976938c94241732e7546409b6e2d9e8f9117effa
Michael Salzarulo <50628598+mikesalzarulo@users.noreply.github.com>
2021-09-07 13:40:38 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us5.api.mailchimp.com/3.0/lists/8fbfbabf72";
+
+    const options = {
+        method: "POST",
+        auth: "michael1:5360073de15bbce791f725596e3c8282-us5"
+    }
+
+    const request = https.request(url, options, function(response){
+            
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            }   else  {
+                res.sendFile(__dirname + "/failure.html");
+            }
+        
+            response.on("data", function(data){
+                console.log(JSON.parse(data));
+            })
+    });
+    
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000");
+}) 
+
+
+// API key
+// 5360073de15bbce791f725596e3c8282-us5
+
+
+// List ID
+// 8fbfbabf72
